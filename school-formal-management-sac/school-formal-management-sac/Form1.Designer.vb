0d17d9c78d0adfe5c77e3aadde7024f08bcc0936
IhsFace <ihsaanifzal75@gmail.com>
2024-02-26 13:07:21 +1100

    Update files

---
+++ school-formal-management-sac/school-formal-management-sac/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtNumStudents = New TextBox()
+        txtNumTeachers = New TextBox()
+        btnReport = New Button()
+        btnClear = New Button()
+        lstDisplay = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 16F)
+        Label1.Location = New Point(320, 28)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(564, 49)
+        Label1.TabIndex = 0
+        Label1.Text = "School Formal Management"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 12F)
+        Label2.Location = New Point(36, 127)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(376, 36)
+        Label2.TabIndex = 1
+        Label2.Text = "Number of student tickets"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 12F)
+        Label3.Location = New Point(35, 192)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(377, 36)
+        Label3.TabIndex = 2
+        Label3.Text = "Number of teacher tickets"
+        ' 
+        ' txtNumStudents
+        ' 
+        txtNumStudents.Font = New Font("Arial", 10F)
+        txtNumStudents.Location = New Point(685, 125)
+        txtNumStudents.Name = "txtNumStudents"
+        txtNumStudents.Size = New Size(450, 38)
+        txtNumStudents.TabIndex = 3
+        ' 
+        ' txtNumTeachers
+        ' 
+        txtNumTeachers.Font = New Font("Arial", 10F)
+        txtNumTeachers.Location = New Point(685, 192)
+        txtNumTeachers.Name = "txtNumTeachers"
+        txtNumTeachers.Size = New Size(450, 38)
+        txtNumTeachers.TabIndex = 4
+        ' 
+        ' btnReport
+        ' 
+        btnReport.Font = New Font("Arial", 10F)
+        btnReport.Location = New Point(35, 280)
+        btnReport.Name = "btnReport"
+        btnReport.Size = New Size(613, 46)
+        btnReport.TabIndex = 5
+        btnReport.Text = "Ticket Sales Report"
+        btnReport.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Arial", 10F)
+        btnClear.Location = New Point(685, 280)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(450, 46)
+        btnClear.TabIndex = 6
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' lstDisplay
+        ' 
+        lstDisplay.BackColor = Color.LemonChiffon
+        lstDisplay.Font = New Font("Arial", 10F)
+        lstDisplay.FormattingEnabled = True
+        lstDisplay.Location = New Point(35, 368)
+        lstDisplay.Name = "lstDisplay"
+        lstDisplay.Size = New Size(1100, 324)
+        lstDisplay.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.IndianRed
+        ClientSize = New Size(1174, 729)
+        Controls.Add(lstDisplay)
+        Controls.Add(btnClear)
+        Controls.Add(btnReport)
+        Controls.Add(txtNumTeachers)
+        Controls.Add(txtNumStudents)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "School Formal Management"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNumStudents As TextBox
+    Friend WithEvents txtNumTeachers As TextBox
+    Friend WithEvents btnReport As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstDisplay As ListBox
+
+End Class
---
+++ school-formal-management-sac/school-formal-management-sac/Form1.vb
@@ -0,0 +1,102 @@
+﻿' PROGRAM NAME: Folio 1
+' PURPOSE: To calculate the total value of tickets sold, applying discounts if applicable, and determine whether the student/teacher ratio meets a specified threshold.
+' AUTHOR: Ihsaan Ifzal
+' DATE CREATED: 26/02/2024
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    ' Constant variables are declared
+    Private Const TicketPricePerStudent As Integer = 100
+    Private Const TicketPricePerTeacher As Integer = 50
+    Private Const DiscountThreshold As Integer = 150
+    Private Const DiscountRate As Double = 0.05
+
+    ' Event handler for the Ticket Sales Report button click.
+    ' Calculates the total value of tickets sold, applies discounts, and displays the results.
+    Private Sub btnReport_Click(sender As Object, e As EventArgs) Handles btnReport.Click
+        ' lstDisplay list box is cleared every time the Ticket Sales Report button is clicked
+        lstDisplay.Items.Clear()
+
+        Dim intNumStudents As Integer
+        Dim intNumTeachers As Integer
+        Dim intTotal As Integer
+
+        ' Validate input for number of students
+        If Not ValidateInput(txtNumStudents, "number of students", intNumStudents) Then Return
+        ' Validate input for number of teachers
+        If Not ValidateInput(txtNumTeachers, "number of teachers", intNumTeachers) Then Return
+
+        ' Calculate total value of tickets sold
+        Try
+            intTotal = (intNumStudents * TicketPricePerStudent) + (intNumTeachers * TicketPricePerTeacher)
+        Catch ex As System.OverflowException
+            ' If the numbers are too large an error message is displayed
+            MessageBox.Show($"The numbers you entered for the number of students and teachers are too large.", "Input Error")
+            txtNumStudents.Clear()
+            txtNumTeachers.Clear()
+            txtNumStudents.Focus()
+            Return
+        End Try
+
+        ' Calculate discount value
+        Dim dblDiscount As Double = If(intNumStudents + intNumTeachers >= DiscountThreshold, intTotal * DiscountRate, 0)
+        ' Calculate student/teacher ratio
+        Dim dblStudTeachRatio As Double = If(intNumTeachers <> 0, intNumStudents / intNumTeachers, 0)
+
+        ' Determine if the student/teacher ratio meets the threshold
+        Dim strStudTeachRatio As String = If(dblStudTeachRatio <= 20, "The Student/Teacher ratio has been met.", "The Student/Teacher ratio has not been met.")
+
+        ' Display results
+        lstDisplay.Items.Add($"{intNumStudents} student tickets and {intNumTeachers} teacher tickets have been sold.")
+        lstDisplay.Items.Add($"The total value of the tickets sold is ${intTotal}.")
+        lstDisplay.Items.Add($"The discount value is ${dblDiscount}.")
+        lstDisplay.Items.Add(strStudTeachRatio)
+    End Sub
+
+    ' Validates input for a TextBox, ensuring it is not empty and contains only whole numbers.
+    ' If validation fails, displays an error message and sets focus to the TextBox.
+    ' textBox Parameter: The TextBox to validate.
+    ' strFieldName Parameter: The name of the field being validated.
+    ' intOutput Parameter: The output variable to store the parsed integer value.
+    ' Returns True if validation succeeds, otherwise False.
+    Private Function ValidateInput(textBox As TextBox, strFieldName As String, ByRef intOutput As Integer) As Boolean
+        Dim strInput As String = textBox.Text.Trim()
+
+        ' Check if input is empty
+        If String.IsNullOrWhiteSpace(strInput) Then
+            MessageBox.Show($"Please provide input for the {strFieldName}.", "Input Error")
+            textBox.Clear()
+            textBox.Focus()
+            Return False
+        End If
+
+        ' Check if input contains only whole numbers
+        If Not Regex.IsMatch(strInput, "^\d+$") Then
+            MessageBox.Show($"Please enter whole numbers for the {strFieldName}.", "Input Error")
+            textBox.Clear()
+            textBox.Focus()
+            Return False
+        End If
+
+        ' Parse input string to integer
+        Try
+            intOutput = Val(strInput)
+        Catch ex As System.OverflowException
+            ' If the numbers are too large an error message is displayed
+            MessageBox.Show($"The number you entered for the {strFieldName} is too large.", "Input Error")
+            textBox.Clear()
+            textBox.Focus()
+            Return False
+        End Try
+
+        Return True
+    End Function
+
+    ' Event handler for the Clear button click.
+    ' Clears the display and input fields.
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstDisplay.Items.Clear()
+        txtNumStudents.Clear()
+        txtNumTeachers.Clear()
+    End Sub
+End Class
